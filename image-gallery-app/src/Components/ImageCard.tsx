0895dc13b22a47ce1eb8124714c184d1fb379f89
unknown <mdamir4298@gmail.com>
2025-03-25 19:04:55 +0530

    imporved style and functionlity of search

--- image-gallery-app/src/Components/ImageCard.tsx
+++ image-gallery-app/src/Components/ImageCard.tsx
@@ -125,11 +125,6 @@ const ImageCard: React.FC = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
-  const handleSearch = (e: React.FormEvent): void => {
-    e.preventDefault();
-    setCurrentPage(1);
-    getData(1);
-  };
 
   const handleFilterChange = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     const { name, value } = e.target;
@@ -140,7 +135,7 @@ const ImageCard: React.FC = () => {
     setCurrentPage(1);
   };
 
-  // Get unique years from images for the year filter
+
   const uniqueYears = Array.from(new Set(images.map(image => image.date.substring(0, 4)))).sort().reverse();
 
   return (
@@ -177,22 +172,40 @@ const ImageCard: React.FC = () => {
             </button>
           </div>
           <div className="search-container">
-            <form onSubmit={handleSearch} className="search-form">
+            <div className="search-form">
               <div className="search-input-container">
+                <div className="search-icon">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                  </svg>
+                </div>
                 <input
                   type="text"
                   placeholder="Search NASA images..."
                   value={searchQuery}
-                  onChange={(e) => setSearchQuery(e.target.value)}
+                  onChange={(e) => {
+                    setSearchQuery(e.target.value);
+                    setCurrentPage(1);
+                    getData(1);
+                  }}
                   className="search-input"
                 />
-                <button type="submit" className="search-button">
-                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                  </svg>
-                </button>
+                {searchQuery && (
+                  <button
+                    onClick={() => {
+                      setSearchQuery('');
+                      setCurrentPage(1);
+                      getData(1);
+                    }}
+                    className="clear-search-button"
+                  >
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
+                )}
               </div>
-            </form>
+            </div>
             <div className="filter-container">
               <select
                 name="year"
